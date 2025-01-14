d597c5a56e7cfa28e3a9587069c66f7a024c132a
OCTAVIO\ourquiola <ourquiola@gmail.com>
2025-01-14 14:29:03 -0500

    Quitando archivos no funcionales y ajustando js de conexion apuntando a env

--- middlewares/withDatabase.js
+++ middlewares/withDatabase.js
@@ -3,8 +3,8 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const { RED_BUCAL_MONGODB_HOTS, RED_BUCAL_MONGODB_DATABASE } = process.env
-//const MONGODB_URI = `mongodb://${RED_BUCAL_MONGODB_HOTS}/${RED_BUCAL_MONGODB_DATABASE}`
-const MONGODB_URI = `mongodb://localhost/red-bucal-database`
+const MONGODB_URI = `mongodb://${RED_BUCAL_MONGODB_HOTS}/${RED_BUCAL_MONGODB_DATABASE}`
+//const MONGODB_URI = `mongodb://localhost/red-bucal-database`
 
 const client = new MongoClient(MONGODB_URI, {
     useUnifiedTopology: true,
--- middlewares/withSession.js
+++ middlewares/withSession.js
@@ -4,8 +4,8 @@ import dotenv from "dotenv";
 dotenv.config()
 
 const { RED_BUCAL_MONGODB_HOTS, RED_BUCAL_MONGODB_DATABASE } = process.env
-const MONGODB_URI = `mongodb://localhost/red-bucal-database`
-//const MONGODB_URI = `mongodb://${RED_BUCAL_MONGODB_HOTS}/${RED_BUCAL_MONGODB_DATABASE}`
+//const MONGODB_URI = `mongodb://localhost/red-bucal-database`
+const MONGODB_URI = `mongodb://${RED_BUCAL_MONGODB_HOTS}/${RED_BUCAL_MONGODB_DATABASE}`
 
 const MongoStore = connectMongo(session)
 
